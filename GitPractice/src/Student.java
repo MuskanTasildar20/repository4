e7902d721aeb477455197a1bb77159e641c2060c
muska <muska@LAPTOP-DB02CP52>
2022-04-27 05:45:43 +1400

    My First Commit for Student

---
+++ GitPractice/src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+	String name;
+	int age;
+	Student(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
